c2998fd49a890fc2b46ab2108353060635b14a04
rgolan-mla <roei.golan@intel.com>
2024-04-05 17:55:39 +0300

    update package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 dependencies = [module for module in open("requirements.txt").read().split("\n")]
 setup(
-    name='HC_feature_selection',
+    name='HC-feature-selection',
     version='0.1',
     packages=find_packages(exclude=['experiments']),
     install_requires=dependencies,
